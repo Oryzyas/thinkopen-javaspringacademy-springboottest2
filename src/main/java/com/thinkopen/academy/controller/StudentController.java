126c5ae63384c54b3a3df0fb4fb2033ae741cfd2
Oryzyas <nicolamartiradonna94@gmail.com>
2018-07-31 12:35:41 +0200

    Spring Boot Test 2

--- src/main/java/com/thinkopen/academy/controller/StudentController.java
+++ src/main/java/com/thinkopen/academy/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.thinkopen.academy.controller;
 import com.thinkopen.academy.entity.Student;
 import com.thinkopen.academy.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +20,10 @@ public class StudentController {
     @RequestMapping(method = RequestMethod.GET)
     public Collection<Student> getAllStudents() {
         return studentService.getAllStudents();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Student getStudentById(@PathVariable("id") int id) {
+        return studentService.getStudentById(id);
     }
 }
--- src/main/java/com/thinkopen/academy/dao/StudentDao.java
+++ src/main/java/com/thinkopen/academy/dao/StudentDao.java
@@ -25,4 +25,8 @@ public class StudentDao {
     public Collection<Student> getAllStudents() {
         return students.values();
     }
+
+    public Student getStudentById(int id) {
+        return students.get(id);
+    }
 }
